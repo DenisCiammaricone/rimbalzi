f4300be8141343084a8856139384bea8ee6b5100
Denis Ciammaricone <d.ciammaricone@outlook.it>
2025-02-14 17:08:48 +0100

    bug fix for server build

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -2,16 +2,17 @@
 import ErrorText from '@/app/components/errorText';
 import { signIn } from "next-auth/react"
 import { AuthError, CredentialsSignin } from 'next-auth';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { set } from 'zod';
 import { useSearchParams } from 'next/navigation';
 
 
 let credentialsError: boolean = false;
 export default function LoginPage() {
-    const searchParams = useSearchParams();
-    credentialsError = searchParams.get("code") === "credentials";
+    //const searchParams = useSearchParams();
+    //credentialsError = searchParams.get("code") === "credentials";
     return (
+        < Suspense >
             <div id="login-register-formBox" className='flex flex-col gap-5 2xl:w-1/6 xl:w-1/4 lg:w-1/4 md:w-1/3 sm:w-full mx-auto p-5 mt-20 '>
                 <h1 className="mx-auto">Login</h1>
                 {/* <form className="flex flex-col gap-2 mx-auto"  action={async (formData) => {
@@ -34,12 +35,13 @@ export default function LoginPage() {
                         <input id="password" name="password" type="password" placeholder="Password" />
                     </div>
                     <button type="submit">Login</button>
-                    {credentialsError && <ErrorText error="Credenziali non valide"></ErrorText>}
+                    {/*credentialsError && <ErrorText error="Credenziali non valide"></ErrorText>*/}
                 </form>
 
                 <hr></hr>
                 <p></p>
                 <div className='mx-auto'>Non hai un account? <a className="link" href='/register'><i>Registrati ora!</i></a></div>
             </div >
+        </Suspense >
     );
 };
