52f13efa6929aac92a61c819313f13186c5e7695
Denis Ciammaricone <d.ciammaricone@outlook.it>
2025-02-14 16:43:24 +0100

    edit next config

--- src/actions/group.ts
+++ src/actions/group.ts
@@ -3,15 +3,15 @@ import { group_permissions } from "@/db/schema/group_permissions";
 import { permissions } from "@/db/schema/permissions";
 import { eq } from "drizzle-orm";
 
-export async function groupHasPermission(groupId: number, perm: string) {
-  const perms = await db.select({id: permissions.id, name: permissions.name})
-  .from(group_permissions)
-  .leftJoin(permissions, eq(permissions.id, group_permissions.id))
-  .where(eq(group_permissions.groupId, groupId));
+export async function groupHasPermission(groupId: number) {
+  const perms = await db.select({ id: permissions.id, name: permissions.name })
+    .from(group_permissions)
+    .leftJoin(permissions, eq(permissions.id, group_permissions.id))
+    .where(eq(group_permissions.groupId, groupId));
 
   if (perms.length === 0) {
     return false;
   }
-  
+
   return true
 }
