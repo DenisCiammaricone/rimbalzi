a944f5f876b54d2e5c75d7b0a37338da1f5fe720
Denis Ciammaricone <d.ciammaricone@outlook.it>
2025-02-14 16:47:07 +0100

    bug fix

--- src/app/(pages)/logout/page.tsx
+++ src/app/(pages)/logout/page.tsx
@@ -3,19 +3,19 @@ import { signOut, useSession } from "next-auth/react"
 import { useRouter } from "next/router";
 import { redirect } from 'next/navigation'
 
-export const logOut = () => {
+export default function logOut() {
     const { data: session, status } = useSession();
-    
-    if(status === "authenticated") {
+
+    if (status === "authenticated") {
         console.log(session)
         return (
             <div className="w-full h-full min-h-screen flex flex-col items-center justify-center">
                 <h1>Logout</h1>
                 <p>Sicuro di voler effettuare il logout?</p>
-                <button onClick={() => {signOut()}}>Si</button>
+                <button onClick={() => { signOut() }}>Si</button>
             </div>
         )
-    } else if(status === "unauthenticated") {
+    } else if (status === "unauthenticated") {
         console.log("No session")
         //redirect("/login")
     } else {
@@ -24,9 +24,7 @@ export const logOut = () => {
                 <svg viewBox="25 25 50 50">
                     <circle r="20" cy="50" cx="50"></circle>
                 </svg>
-            </div> 
+            </div>
         )
     }
 }
-
-export default logOut
