48cc37738a434e1230fd537d4702592ea290efde
Denis Ciammaricone <d.ciammaricone@outlook.it>
2025-03-31 14:29:55 +0200

    Logout bug Fix (Wrong Redirect)

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+	reactStrictMode: true,
+	env: {
+		NEXTAUTH_URL: 'https://labinformaticaescuola.it',
+	},
+	eslint: {
+    	ignoreDuringBuilds: true,
+    }
 };
-
-module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
-}
 
 export default nextConfig;
